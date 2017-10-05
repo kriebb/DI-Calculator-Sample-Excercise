e97c6dd748e22e182e08d67a80ba751abd0f2688
kriebb <kristof.riebbels@recomatics.com>
2017-10-05 16:20:02 +0200

    Functionaliteit Add toevoegen (+), voor Multiply kies je de operator (+). Indien een operator niet gekend, dan gaat de applicatie in exceptie

--- DI Calculator/Program.cs
+++ DI Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,41 @@ namespace DI_Calculator
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            //Werk nu met een methode, zodanig dat je geen logging moet schrijven dat je x met y *
+            //Zorg ervoor dat je applicatie werkt met 'Add', naast ook Multiply te kennen, op basis van een parameter. Multiply = * en Add = +
+            //Zorg ervoor dat je 4+3 uitkomt
+            //Indien een niet gekende operatie gebruikt wordt, gaat de applicatie een exception smijten.
+            var operation = "+";
+
             var x = 4;
             var y = 3;
-            var multiplied = Multiply(x, y);
-            Console.WriteLine(multiplied);
+            int result = 0;
+            switch (operation)
+            {
+                case "*":
+                    result = Multiply(x, y);
+                    break;
+                case "+":
+                    result = Add(x, y);
+                    break;
+                default:
+                    throw new ArgumentException($"We ondersteunen niet de operatie: {operation} enkel maar de operatie * of +");
+            }
+
+            Console.WriteLine(result);
             Console.ReadKey();
         }
 
         static int Multiply(int x, int y)
         {
             return x * y;
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
