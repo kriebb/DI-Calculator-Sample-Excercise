e4370288fb4d698c4c11b113019ec5c6334c5875
kriebb <kristof.riebbels@recomatics.com>
2017-10-11 13:29:26 +0200

    Resultaat van opdracht: //Verwijderen van oude referentie Unity (foutje bij setup by me).             //Toevoegen van unity op project DI Calculator             //Splits het gebruik van de container op in twee delen:             //-Registraties             //-Resolving             //Gebruik die twee zaken nooit door elkaar.             //Werk altijld met een child container             //In een childcontainer kan je ook je registraties overschrijven.             //Vervang de manuele injectie door een resolve op de container.             //Registreer nog niet de interface IOutputBuilderResult met de gekozen implementatie             //Run

--- DI Calculator/Program.cs
+++ DI Calculator/Program.cs
@@ -4,8 +4,11 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using DI_Calculator.Bootstrap;
+using DI_Calculator.CalculationManagement;
 using DI_Calculator.CalculationManagement.Impl;
+using DI_Calculator.DisplayManagement;
 using DI_Calculator.DisplayManagement.Impl;
+using Microsoft.Practices.Unity;
 using ObscureWare.Console;
 
 namespace DI_Calculator
@@ -13,9 +16,25 @@ namespace DI_Calculator
     class Program
     {
 
+        static void Register(IUnityContainer unityContainer)
+        {
+            unityContainer.RegisterType<IConsole, SystemConsole>(new ContainerControlledLifetimeManager(), new InjectionConstructor(new ConsoleController()));
+            unityContainer.RegisterType<IDisplayer, ResultDisplayer>(new ContainerControlledLifetimeManager());
+            unityContainer.RegisterType<ICalculator, SimpleCalculator>(new ContainerControlledLifetimeManager());
+        }
         static void Main(string[] args)
         {
-
+            //Verwijderen van oude referentie Unity (foutje bij setup by me).
+            //Toevoegen van unity op project DI Calculator
+            //Splits het gebruik van de container op in twee delen:
+            //-Registraties
+            //-Resolving
+            //Gebruik die twee zaken nooit door elkaar.
+            //Werk altijld met een child container
+            //In een childcontainer kan je ook je registraties overschrijven.
+            //Vervang de manuele injectie door een resolve op de container.
+            //Registreer nog niet de interface IOutputBuilderResult met de gekozen implementatie
+            //Run
 
             var operation1 = "+";
 
@@ -26,15 +45,21 @@ namespace DI_Calculator
             var x2 = 4;
             var y2 = 3;
 
-            var console = new SystemConsole(new ConsoleController());
+            using (var unityContainer = new UnityContainer())
+            {
+                Register(unityContainer);
 
-            var calculator = new SimpleCalculator();
-            var displayer = new ResultDisplayer(console, new ResultFactory());
-            var appStartup = new Startup(calculator, displayer);
+                //You can also resolve here some stuff... This is the main.
+                //You can put this in a loop....
+                using (var childContainer = unityContainer.CreateChildContainer())
+                {
+                    //You can also register here some stuff if you want to override
+                    var appStartup = childContainer.Resolve<Startup>();
 
-            appStartup.Start(operation1, x1, y1);
-            appStartup.Start(operation2, x2, y2);
-
+                    appStartup.Start(operation1, x1, y1);
+                    appStartup.Start(operation2, x2, y2);
+                }
+            }
         }
 
 
